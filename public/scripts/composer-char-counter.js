2ab0c1dcc3eeec20c0f8bb4db421131931a2edfd
Katie Liu <katieliu59@gmail.com>
2023-01-31 00:04:31 -0500

    Fix bug where if/else both took into account backspace key to increase and decrese count

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,32 +1,31 @@
-$(document).ready(function() {
+$(document).ready(function () {
+	let count = 140;
+	$("textarea").on("keydown", function (event) {
+		// console.log($(this).val().length)
+		// console.log(e)
 
-    let count = 140;
-    $("textarea").on("keydown", function(e) {
-        console.log($(this).val().length)
-        console.log(e)
-        // count--;
-        // $(".counter").text(count);
-        // if (count < 0) {
-        //     //add error class to make number color red
-        // }
-        
-        count -= $(this).val().length;
-        $(this).next('.tweet-btn-counter').children(".counter").text(count);
-        
-        if (e.key === 'Backspace' && count < 140) {
-            count++;
-            $(this).next('.tweet-btn-counter').children(".counter").text(count);
-        }
+		//Increase counter when press "Backspace" and decrement counter when typing
+		if (event.key === "Backspace" && count < 140) {
+			count++;
+			$(this).next(".tweet-btn-counter").children(".counter").text(count);
+		} else if (event.key === "Backspace" && count === 140) {
+			return false;
+		} else {
+			count--;
+			$(this).next(".tweet-btn-counter").children(".counter").text(count);
+		}
 
-        if (count < 0) {
-            $(this).next('.tweet-btn-counter').children(".counter").addClass("error");
-        }
+		//Add error class to make count number red when count is below 0
+		if (count < 0) {
+			$(this).next(".tweet-btn-counter").children(".counter").addClass("error");
+		}
 
-        if (count >= 0) {
-            $(this).next('.tweet-btn-counter').children(".counter").removeClass("error");
-        }
-
-    })
-   
-
+		//Remove error class that makes count number red when count is 0 or above
+		if (count >= 0) {
+			$(this)
+				.next(".tweet-btn-counter")
+				.children(".counter")
+				.removeClass("error");
+		}
+	});
 });
