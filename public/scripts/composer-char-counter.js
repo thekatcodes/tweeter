e344a6f28ee846d70ff9d61e9650b4d9bd866cd7
Katie Liu <katieliu59@gmail.com>
2023-01-30 23:39:33 -0500

    Implement logic to countdown from 140 by character input

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,32 @@
 $(document).ready(function() {
-    // --- our code goes here ---
-  });
+
+    let count = 140;
+    $("textarea").on("keydown", function(e) {
+        console.log($(this).val().length)
+        console.log(e)
+        // count--;
+        // $(".counter").text(count);
+        // if (count < 0) {
+        //     //add error class to make number color red
+        // }
+        
+        count -= $(this).val().length;
+        $(this).next('.tweet-btn-counter').children(".counter").text(count);
+        
+        if (e.key === 'Backspace' && count < 140) {
+            count++;
+            $(this).next('.tweet-btn-counter').children(".counter").text(count);
+        }
+
+        if (count < 0) {
+            $(this).next('.tweet-btn-counter').children(".counter").addClass("error");
+        }
+
+        if (count >= 0) {
+            $(this).next('.tweet-btn-counter').children(".counter").removeClass("error");
+        }
+
+    })
+   
+
+});
